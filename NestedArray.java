988e9972c8e87384634ae4f0540438d7af421bb3
hitesh <hiteshkukaramprajapati@gmail.com>
2024-11-26 20:57:26 +0530

    Simple problem with Nested Arrays

--- NestedArray.java
+++ NestedArray.java
@@ -1,3 +1,31 @@
+import java.util.Arrays;
 public class NestedArray {
-    ma
+    public static void main(String[] args) {
+        int[][] a= new int[3][3];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if(i==j){
+                    a[i][j] =1;
+                }
+                else{
+                    a[i][j]=i;
+                }
+            }
+        }
+        for (int i = 0; i < a.length; i++) {   
+            System.out.println(Arrays.toString(a[i]));
+        }
+
+        int[][] b = new  int[a[0].length][a.length];
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                b[i][j]=a[j][i];
+            }
+        }
+        for (int i = 0; i < b.length; i++) {   
+            System.out.println(Arrays.toString(b[i]));
+        }
+
+
+    }
 }
